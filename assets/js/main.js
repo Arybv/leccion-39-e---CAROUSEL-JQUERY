188d3e1d8923e41cd40e051d2cc2a0436defe95a
Arantza <arybv18@gmail.com>
2017-06-05 07:23:36 -0500

    modificando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,41 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
+  var sliders = $("#sliders");
+  var antes = $("#antes");
+  var despues = $("#despues");
 
+$("#sliders figure:last").insertBefore("#sliders figure:first");
+sliders.css("margin-left", "-"+100+"%");
+
+function posicionAntes(){
+  slider.animate({
+    marginLeft:'-'+200+'%'
+  } , 700, function(){
+    $('#slider figure:first').insertAfter('#slider figure:last');
+    slider.css('margin-left', '-'+100+'%');
+  });
+}
+
+function posicionDespues(){
+  sliders.animate({
+    marginLeft:"-"+200+"%"
+  }, 700, function(){
+    $("#sliders figure:last").insertAfter("#sliders figure:firts");
+    sliders.css("margin-left", "-"+100+"%");
+  });
+}
+
+despues.on("click", function(){
+    posicionAntes();
 });
-*/
-$(function(){
-  $("#inicio").html("Iniciamos la partida")
+
+antes.on("click", function(){
+    posicionDespues();
 });
+// SLIDER-AUMATIC
+function automatic(){
+  setInterval(function () {
+  posicionDespues();
+}, 7000);
+}
+automatic();
+})
